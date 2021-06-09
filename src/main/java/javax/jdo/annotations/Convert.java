ffa97295f6fc61ddeea6b6471be4e41c7fc607d9
andyjefferson <andy@datanucleus.org>
2021-06-09 13:05:25 +0100

    Drop @Converts. Fixes #19

--- src/main/java/javax/jdo/annotations/Convert.java
+++ src/main/java/javax/jdo/annotations/Convert.java
@@ -17,7 +17,6 @@
 package javax.jdo.annotations;
 
 import java.lang.annotation.ElementType;
-import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -37,7 +36,6 @@ import javax.jdo.AttributeConverter;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
-@Repeatable(Converts.class)
 public @interface Convert {
 
 	/**
